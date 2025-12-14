2d5e0b83184d8373005b6ccc897f51faab9150b5
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-12-14 21:14:49 +0100

    testing API_URN

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
+  console.log(process.env.API_URL)
   return (
     <div>
       <Hero />
