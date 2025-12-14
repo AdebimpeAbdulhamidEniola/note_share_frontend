88258adbf0f6fa92faeedc17652e215acb0e1abf
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-12-14 19:50:39 +0100

    include environment variable to connect to backend

--- lib/api.ts
+++ lib/api.ts
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: " http://localhost:2004/api/snippets", 
+  baseURL: process.env.API_URL || " http://localhost:2004/api/snippets", 
   timeout: 15000,
   headers: { "Content-Type": "application/json" },
 });
