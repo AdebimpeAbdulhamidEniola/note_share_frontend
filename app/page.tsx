eb9cb42bbfcf61ec6c166c9e4ee6cf637361507f
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-12-15 08:28:13 +0100

    fixed environment variable naming in NextJs

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
-console.log(`This is the api url ${process.env.API_URL}`)
+console.log(`This is the api url ${process.env.NEXT_PUBLIC_API_URL}`)
 
   return (
     <div>
