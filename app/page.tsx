cbca7738317cb7ea168cc8051a538a1af9003c54
AdebimpeAbdulhamidEniola <adebimpeabdulhamid5@gmail.com>
2025-12-15 08:36:25 +0100

    add security features to external URL

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import Header from "@/components/Header";
 import Hero from "@/components/Hero";
 
 export default function Home() {
-console.log(`This is the api url ${process.env.NEXT_PUBLIC_API_URL}`)
 
   return (
     <div>
